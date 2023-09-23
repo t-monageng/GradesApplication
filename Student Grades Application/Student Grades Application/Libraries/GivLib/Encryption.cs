ffa7f5a93d3c59012da2cae89217a620e74b598d
Tshiamo Monageng <90952386+snw-hub@users.noreply.github.com>
2023-09-23 21:11:05 +0200

    Add files via upload

---
+++ Student Grades Application/Student Grades Application/Libraries/GivLib/Encryption.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GivLib
+{
+    public class Encryption
+    {
+        public string EncryptString(string plainText, string key)
+        {
+            using (AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                aesAlg.GenerateIV();
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+
+                    byte[] iv = aesAlg.IV;
+                    byte[] encryptedBytes = msEncrypt.ToArray();
+
+                    // Combine IV and encrypted data
+                    byte[] combinedBytes = new byte[iv.Length + encryptedBytes.Length];
+                    Array.Copy(iv, 0, combinedBytes, 0, iv.Length);
+                    Array.Copy(encryptedBytes, 0, combinedBytes, iv.Length, encryptedBytes.Length);
+
+                    return Convert.ToBase64String(combinedBytes);
+                }
+            }
+        }
+
+        static string DecryptString(string cipherText, string key)
+        {
+            byte[] combinedBytes = Convert.FromBase64String(cipherText);
+
+            using (AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                byte[] iv = new byte[16];
+                Array.Copy(combinedBytes, 0, iv, 0, iv.Length);
+                aesAlg.IV = iv;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream())
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Write))
+                    {
+                        csDecrypt.Write(combinedBytes, iv.Length, combinedBytes.Length - iv.Length);
+                    }
+
+                    byte[] decryptedBytes = msDecrypt.ToArray();
+                    return Encoding.UTF8.GetString(decryptedBytes);
+                }
+            }
+        }
+
+      
+    }
+}
---
+++ Student Grades Application/Student Grades Application/MainWindow.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using GivLib;
+
+namespace Student_Grades_Application
+{
+    public partial class MainWindow : Window
+    {
+        string Filename = "";
+        string filePath = "";
+        Encryption enc = new Encryption();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Backup();
+        }
+
+        private void CreateFile_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Creating a new txt file using dedicated textbox
+                if (txtFileName.Text != null | txtFileName.Text != "")
+                {
+                    Encryption enc = new Encryption();
+                    //Filename = enc.EncryptString(txtFileName.Text, "MySecretKey12345");
+                    Filename = $"{Filename}.txt";
+
+                    string folderPath = @"C:\StudentGradesApp\Files"; // Replace with your desired folder path
+                    Directory.CreateDirectory(folderPath);
+
+
+                    filePath = Path.Combine(folderPath, Filename); // Specify the file name and extension
+                    File.Create(filePath).Close();
+
+
+
+
+                    //  File.Create(Filename).Close();
+                    //File.Encrypt(Filename);
+
+                    //Notify file created and clear
+                    AddToCombo();
+                    MessageBox.Show($"{txtFileName.Text} created successfully :)");
+                    txtFileName.Text = "";
+                }
+            }
+            catch (Exception exc)
+            {
+
+                MessageBox.Show("Error: " + exc.Message);
+            }
+        }
+
+
+        private void Display_Click(object sender, RoutedEventArgs e)
+        {
+            // Read the contents of grades.txt using LINQ and display them in the DataGridView
+            List<string[]> data = File.ReadAllLines(filePath)
+                .Select(line => line.Split(','))
+                .ToList();
+
+            // Bind the data to the DataGridView
+            DataGridView.ItemsSource = data;
+        }
+        private void AddToFile_Click(object sender, RoutedEventArgs e)
+        {
+            Encryption enc = new Encryption();
+            File.Decrypt(Filename);
+
+
+            //Filename = enc.DecryptString(Filename,"MySecretKey12345");
+
+            // Add data from textboxes to grades.txt
+            string studentNumber = StudentNumberTextBox.Text;
+            string studentName = StudentNameTextBox.Text;
+            string surname = SurnameTextBox.Text;
+            string subject = SubjectTextBox.Text;
+            string indivAssignGrade = IndivAssignGradeTextBox.Text;
+            string groupAssignGrade = GroupAssignGradeTextBox.Text;
+            string testGrade = TestGradeTextBox.Text;
+
+            string dataToWrite = $"{studentNumber},{studentName},{surname},{subject},{indivAssignGrade},{groupAssignGrade},{testGrade}";
+
+            File.AppendAllText(Filename, dataToWrite + Environment.NewLine);
+
+            // Clear textboxes after adding data
+            ClearTextBoxes();
+        }
+
+        private void ClearTextBoxes()
+        {
+            StudentNumberTextBox.Clear();
+            StudentNameTextBox.Clear();
+            SurnameTextBox.Clear();
+            SubjectTextBox.Clear();
+            IndivAssignGradeTextBox.Clear();
+            GroupAssignGradeTextBox.Clear();
+            TestGradeTextBox.Clear();
+        }
+
+        private void Backup()
+        {
+            string listPath = @"C:\StudentGradesApp\Backup"; // Replace with your desired folder path
+            Directory.CreateDirectory(listPath);
+
+
+            filePath = Path.Combine(listPath, "File list.txt" ); // Specify the file name and extension
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+            }
+
+
+            AddToCombo();
+
+        }
+        private void AddToCombo()
+        {
+            //adding 
+            string folderPath = @"C:\StudentGradesApp\Files"; // Replace with your folder path
+            string outputFile = @"C:\StudentGradesApp\Backup\FileList"; // Replace with your output file path
+
+            string[] files = Directory.GetFiles(folderPath);
+
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                foreach (string file in files)
+                {
+                    writer.WriteLine(file);
+                }
+            }
+
+            combFileList.Items.Clear();
+
+            // Add each file name to the ComboBox
+            foreach (string file in files)
+            {
+                // You can choose to display only the file name or the full path
+                // by using Path.GetFileName(file) or file, respectively.
+                combFileList.Items.Add(Path.GetFileName(file));
+            }
+        }
+    }
+}
+
